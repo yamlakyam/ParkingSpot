fc0e101bac371094b2402548dc4331168a48c417
yam <yamlakyam@gmail.com>
2020-10-27 16:50:34 +0300

    added another map on the menu-using activity

--- app/src/main/java/com/gebeya/parkingspot/DisplayMapsActivity.kt
+++ app/src/main/java/com/gebeya/parkingspot/DisplayMapsActivity.kt
@@ -1,30 +1,133 @@
 package com.gebeya.parkingspot
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.location.*
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.activity_display_maps.*
 
-class DisplayMapsActivity : AppCompatActivity() {
+class DisplayMapsActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+
+    private lateinit var map: MapboxMap
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var locationEngine: LocationEngine
+    private lateinit var callback: LocationChangeListeningCallback
+    private val DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L
+    private val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        // Mapbox access token is configured here. This needs to be called either in your application
-// object or in the same activity which contains the mapview.
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-
-// This contains the MapView in XML and needs to be called after the access token is configured.
         setContentView(R.layout.activity_display_maps)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        map = mapboxMap
+        callback = LocationChangeListeningCallback()
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            enableLocationComponent(it)
+        }
+    }
 
-// Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .useDefaultLocationEngine(false)
+                .build()
+            map.locationComponent.apply {
+                activateLocationComponent(locationComponentActivationOptions)
+                isLocationComponentEnabled = true                       // Enable to make component visible
+                cameraMode = CameraMode.TRACKING                        // Set the component's camera mode
+                renderMode = RenderMode.COMPASS                         // Set the component's render mode
             }
+            initLocationEngine()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private fun initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this)
+        val request = LocationEngineRequest
+            .Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+            .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME)
+            .build()
+        locationEngine.requestLocationUpdates(request, callback, mainLooper)
+        locationEngine.getLastLocation(callback)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    private inner class LocationChangeListeningCallback :
+        LocationEngineCallback<LocationEngineResult> {
+
+        override fun onSuccess(result: LocationEngineResult?) {
+            result?.lastLocation ?: return //BECAREFULL HERE, IF NAME LOCATION UPDATE DONT USER -> val resLoc = result.lastLocation ?: return
+            if (result.lastLocation != null){
+                val lat = result.lastLocation?.latitude!!
+                val lng = result.lastLocation?.longitude!!
+                val latLng = LatLng(lat, lng)
+
+                if (result.lastLocation != null) {
+                    map.locationComponent.forceLocationUpdate(result.lastLocation)
+                    val position = CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(13.0) //disable this for not follow zoom
+                        .tilt(10.0)
+                        .build()
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(position))
+                    Toast.makeText(this@DisplayMapsActivity, "Location update : $latLng", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
+        override fun onFailure(exception: Exception) {}
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, "Permission needed", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            map.getStyle {
+                enableLocationComponent(it)
+            }
+        } else {
+            Toast.makeText(this, "Permission not granted!! app will be EXIT", Toast.LENGTH_LONG).show()
+            Handler().postDelayed({
+                finish()
+            }, 3000)
         }
     }
 
@@ -62,5 +165,11 @@ class DisplayMapsActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         mapView?.onSaveInstanceState(outState)
     }
+
+
+
+
+
+
 
 }
