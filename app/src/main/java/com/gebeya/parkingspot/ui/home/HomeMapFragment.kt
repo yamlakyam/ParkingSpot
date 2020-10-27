d67cfa1b3558a39391814e8dc33d06f41c48888d
yam <yamlakyam@gmail.com>
2020-10-27 14:44:55 +0300

    device location added --code inside fragment

--- app/src/main/java/com/gebeya/parkingspot/ui/home/HomeMapFragment.kt
+++ app/src/main/java/com/gebeya/parkingspot/ui/home/HomeMapFragment.kt
@@ -1,22 +1,41 @@
 package com.gebeya.parkingspot.ui.home
 
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gebeya.parkingspot.R
+import com.mapbox.android.core.location.*
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.fragment_home_map.*
+import java.lang.ref.WeakReference
 
-class HomeMapFragment : Fragment() {
+class HomeMapFragment : Fragment(), OnMapReadyCallback, PermissionsListener {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var locationEngine: LocationEngine
+    private val DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L
+    private val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5
+    private var callback: HomeMapFragmentLocationCallback = HomeMapFragmentLocationCallback(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,6 +57,94 @@ class HomeMapFragment : Fragment() {
         mapView.getMapAsync{
             it.setStyle(Style.MAPBOX_STREETS)
         }
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap =mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            enableLocationComponent(it)
+        }
+
+    }
+    @SuppressWarnings("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(requireContext())) {
+            val locationComponent = mapboxMap.locationComponent
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(requireContext(), loadedMapStyle)
+                .useDefaultLocationEngine(false)
+                .build()
+            locationComponent.apply {
+                activateLocationComponent(locationComponentActivationOptions)
+                isLocationComponentEnabled = true                       // Enable to make component visible
+                cameraMode = CameraMode.TRACKING                        // Set the component's camera mode
+                renderMode = RenderMode.COMPASS                         // Set the component's render mode
+            }
+            initLocationEngine()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager!!.requestLocationPermissions(requireActivity())
+        }
+    }
+    @SuppressWarnings( "MissingPermission")
+    private fun initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(requireContext())
+        val request = LocationEngineRequest
+            .Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+            .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME)
+            .build()
+        locationEngine.requestLocationUpdates(request, callback, Looper.getMainLooper())
+        locationEngine.getLastLocation(callback)
+
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(requireContext(), "Permission not granted!!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            mapboxMap.getStyle {
+                enableLocationComponent(it)
+            }
+        } else {
+            Toast.makeText(requireContext(), "Permission not granted!! app will be EXIT", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    inner class HomeMapFragmentLocationCallback internal constructor(fragment: HomeMapFragment) :
+        LocationEngineCallback<LocationEngineResult> {
+        private val fragmentWeakReference: WeakReference<HomeMapFragment> = WeakReference(fragment)
+
+        override fun onSuccess(result: LocationEngineResult?) {
+            val fragment: HomeMapFragment? = fragmentWeakReference.get()
+            if (fragment != null) {
+                val location = result?.lastLocation ?: return
+                val lat = result.lastLocation?.latitude!!
+                val lng = result.lastLocation?.longitude!!
+                val latLng = LatLng(lat, lng)
+
+                Toast.makeText(requireContext(), "Location update : $latLng", Toast.LENGTH_SHORT).show()
+
+                if (fragment.mapboxMap != null && result.lastLocation != null) {
+                    fragment.mapboxMap.getLocationComponent()
+                        .forceLocationUpdate(result.lastLocation)
+                }
+            }
+        }
+
+        override fun onFailure(exception: java.lang.Exception) {
+            Log.d("LocationChangeActivity", exception.localizedMessage)
+            val fragment: HomeMapFragment? = fragmentWeakReference.get()
+            if (fragment != null) {
+                Toast.makeText(requireContext(), exception.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 
